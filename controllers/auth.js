95e5a6f0d8cad74478c3b19ecdbb9311a45a31c6
Luis Herrera <luis1818.lh@gmail.com>
2024-05-08 18:28:34 -0500

    Update with middlewares into post login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,23 @@
 const express = require('express');
+const {validationResult} = require('express-validator')
 
 const createUser = (req, res = express.response) => {
 
     const { name, email, password } = req.body
 
-    if(name.length <5){
-        return res.status(400).json({
-            ok: false,
-            msg: 'El nombre debe ser de 5 letras'
-        })
+    //manejo de errores
+
+    const errors = validationResult(req)
+    console.log(errors)
+    if(!errors.isEmpty()){
+      return res.status(400).json({
+        ok: false,
+        errors: errors.mapped()
+      })
+
     }
 
-    res.json({
+    res.status(201).json({
         ok: true,
         msg: 'register',
         name,
@@ -24,7 +30,16 @@ const loginUser = (req, res) => {
 
     const { email, password } = req.body
 
-    res.json({
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        })
+    }
+
+    res.status(201).json({
         ok: true,
         msg: 'login',
         email,
